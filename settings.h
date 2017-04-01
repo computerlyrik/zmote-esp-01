4c562e961fb7f00abd68815e30e4d300b14f80e6
Christian Fischer <sw-dev@computerlyrik.de>
2017-04-01 14:22:28 +0200

    add settings file

---
+++ settings.h
@@ -0,0 +1,11 @@
+#ifndef _SETTINGS_H_
+#define _SETTINGS_H_
+
+const char* ssid = "**********";
+const char* password = "********";
+
+#define WEB_PORT 80
+#define UDP_PORT 4333
+#define MDNS_NAME "zmote"
+
+#endif
