dfc5b04fe9838434b142720b0d45c4782b9b6e8b
Christian Fischer <sw-dev@computerlyrik.de>
2017-04-01 22:21:07 +0200

    cleanup and readme update

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Firmware for esp-01 aiming to support ZMote v2 API
 
 Aims to work with https://github.com/alexmaret/openhab-binding-zmote Using a mini-setup with a ESP-01
+
+Uses:
+
+https://github.com/markszabo/IRremoteESP8266
+
--- uuid.ino
+++ uuid.ino
@@ -15,39 +15,4 @@ void get_uuid(char* uuid) {
 }
 
 
-void get_uuid_via_eeprom(char* uuid) {
-  //Serial.println("Testing UUID Startbyte from EEPROM");
-
-  byte startbyte = EEPROM.read(0);
-  if (startbyte == 0xFF || startbyte == 0x00) {
-    //Serial.println("Found ");
-    //Serial.print(startbyte);
-    //Serial.println(" - generating UUID");
-    set_uuid_by_mac();
-  }
-  
-  for (int i = 0; i<UUID_SIZE; i++) {
-    sprintf(uuid,"%s%02x",uuid,EEPROM.read(i));
-  }
-  //Serial.print("Read UUID from EEPROM: ");
-  //Serial.println(uuid);
-
-}
-
-
-void set_uuid_by_mac() {
-
-    uint8_t MAC_array[6];
-    WiFi.macAddress(MAC_array);
-    //Serial.print("Writing to eeprom: ");
-    
-    for (int i = 0; i < sizeof(MAC_array); ++i){
-      //Serial.print(MAC_array[i]);
-      EEPROM.write(i, MAC_array[i]);
-    }
-    //Serial.println();
-}
-
-
-
 
